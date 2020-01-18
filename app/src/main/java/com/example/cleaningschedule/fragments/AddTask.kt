5d194bfd6c9ad7b0a1feda48a04bff8bb722bacd
Niamh Carter <niamh.carter13@gmail.com>
2020-01-18 20:33:19 +0000

    Added views to add task page

--- app/src/main/java/com/example/cleaningschedule/fragments/AddTask.kt
+++ app/src/main/java/com/example/cleaningschedule/fragments/AddTask.kt
@@ -1,13 +1,17 @@
 package com.example.cleaningschedule.fragments
 
+import android.content.Context
+import android.graphics.Color
 import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import com.example.cleaningschedule.viewmodels.AddTaskViewModel
 import com.example.cleaningschedule.R
+import kotlinx.android.synthetic.main.add_task_fragment.*
 
 
 class AddTask : Fragment() {
@@ -22,6 +26,10 @@ class AddTask : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
+//        taskLabel.setText("${R.string.task}*")
+//        taskLabel.setTextColor(Color.RED)
+
         return inflater.inflate(R.layout.add_task_fragment, container, false)
     }
 
@@ -30,4 +38,12 @@ class AddTask : Fragment() {
         viewModel = ViewModelProviders.of(this).get(AddTaskViewModel::class.java)
         // TODO: Use the ViewModel
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+//        taskImageView.layoutParams.width = taskImageView.height
+    }
+
+    
 }
--- app/src/main/java/com/example/cleaningschedule/viewmodels/AddTaskViewModel.kt
+++ app/src/main/java/com/example/cleaningschedule/viewmodels/AddTaskViewModel.kt
@@ -3,5 +3,6 @@ package com.example.cleaningschedule.viewmodels
 import androidx.lifecycle.ViewModel
 
 class AddTaskViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+
+
 }
